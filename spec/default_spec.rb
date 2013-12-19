bf7a217f90c1b27b1d5a7b1f992a596e0190ea2c
Darron Froese <darron@froese.org>
2013-12-18 22:40:38 -0700

    Got rid of some trailing spaces.

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -16,11 +16,11 @@ describe 'octohost::default' do
   it 'includes the `nodejs` recipe' do
     expect(chef_run).to include_recipe('nodejs::default')
   end
-  
+
   it 'includes the `hipache` recipe' do
     expect(chef_run).to include_recipe('hipache::default')
   end
-  
+
   it 'includes the `serf` recipe' do
     expect(chef_run).to include_recipe('serf::default')
   end
@@ -28,7 +28,7 @@ describe 'octohost::default' do
   it 'includes the `gitreceive` recipe' do
     expect(chef_run).to include_recipe('gitreceive::default')
   end
-  
+
   before do
     stub_command("hipache -h | grep 'hipache server'").and_return(true)
   end
