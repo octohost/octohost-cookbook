e35ff39df985573a652f20757cc3efaabd3b04d7
Darron Froese <darron@froese.org>
2014-04-13 15:33:26 -0600

    Adding gzip to outbound proxied requests.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer       'Darron Froese'
 maintainer_email 'darron@froese.org'
 license          'Apache 2.0'
 description      'Installs/configures octohost'
-version          '0.13.0'
+version          '0.14.0'
 recipe           'octohost::default', 'Installs/configures octohost'
 
 depends 'chef-sugar'
