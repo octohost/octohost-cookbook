1976d684457163c26aa656b3c322ec5c41e8a309
Darron Froese <darron@froese.org>
2014-05-08 10:13:43 -0600

    Handled now in recipes/vagrant.rb where it should be.

--- vagrant-cleanup.sh
+++ vagrant-cleanup.sh
@@ -2,8 +2,6 @@
 #
 # Disable UFW
 ufw disable
-# Fix the PRIVATE_IP link:
-sed -i '5s/.*/PRIVATE_IP=\"192\.168\.62\.86\"/' /etc/default/octohost
 # Cleanup to shrink vagrant images.
 apt-get -y autoremove
 apt-get -y clean
