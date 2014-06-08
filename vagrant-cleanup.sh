e8c76c5e34149bdbd591dfbbaf4d1d83c56b7ff3
Darron Froese <darron@froese.org>
2014-06-07 19:21:25 -0600

    Add a few more items to make sure it's all ready for distribution.

--- vagrant-cleanup.sh
+++ vagrant-cleanup.sh
@@ -2,7 +2,12 @@
 #
 # Disable UFW
 ufw disable
+# Restart tentacles to get the right IP address.
+octo tentacles stop
+octo tentacles start
 # Cleanup to shrink vagrant images.
+apt-get update
+apt-get upgrade -y
 apt-get -y autoremove
 apt-get -y clean
 dd if=/dev/zero of=/EMPTY bs=1M
