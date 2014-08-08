26d3a41c6de7c4c93dcbf4f89224662ded4ebd8f
Darron Froese <darron@froese.org>
2014-08-08 16:15:02 -0700

    Images don't need to be so big.

--- vagrant-cleanup.sh
+++ vagrant-cleanup.sh
@@ -3,8 +3,8 @@
 # Disable UFW
 ufw disable
 # Cleanup to shrink vagrant images.
-apt-get update
-apt-get upgrade -y
+#apt-get update
+#apt-get upgrade -y
 apt-get -y autoremove
 apt-get -y clean
 dd if=/dev/zero of=/EMPTY bs=1M
