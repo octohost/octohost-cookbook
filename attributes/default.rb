2890ca4808f522d6401530947af96daa9675fe1b
Darron Froese <darron@froese.org>
2015-01-06 19:26:13 -0700

    Don't need those attributes - they're wrong.

--- attributes/default.rb
+++ attributes/default.rb
@@ -26,8 +26,6 @@ default['ruby']['version'] = '2.1'
 default['consul']['encrypt'] = 'p4T1eTQtKji/Df3VrMMLzg=='
 default['consul']['install_method'] = 'binary'
 default['consul']['data_dir'] = '/var/cache/octohost'
-default['consul']['bind_addr'] = '192.168.62.86'
-default['consul']['client_address'] = '192.168.62.86'
 default['consul']['datacenter'] = 'dc1'
 default['consul']['enable_syslog'] = true
 default['consul']['node_name'] = 'octodev-master'
