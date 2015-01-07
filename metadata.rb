07c6addc73b4d408cc75600cb41831a42318b6b9
Darron Froese <darron@froese.org>
2015-01-06 19:26:24 -0700

    Bump version.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer       'Darron Froese'
 maintainer_email 'darron@froese.org'
 license          'Apache 2.0'
 description      'Installs/configures octohost'
-version          '0.29.0'
+version          '0.30.0'
 recipe           'octohost::default', 'Installs/configures octohost'
 
 depends 'chef-sugar'
