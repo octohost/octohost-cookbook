b8ff72198e83df3e316ca5cc84995a11c3bc3378
Darron Froese <darron@froese.org>
2015-02-13 12:44:24 -0700

    Fix Serverspec for current version.

--- test/integration/default/serverspec/spec_helper.rb
+++ test/integration/default/serverspec/spec_helper.rb
@@ -1,11 +1,5 @@
-# encoding: utf-8
+# Encoding: utf-8
 require 'serverspec'
 
-include Serverspec::Helper::Exec
-include Serverspec::Helper::DetectOS
-
-RSpec.configure do |c|
-  c.before :all do
-    c.path = '/sbin:/usr/sbin'
-  end
-end
+set :backend, :exec
+set :path, '/sbin:/usr/local/sbin:$PATH'
