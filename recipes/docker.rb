d55ab48eab905c96e1a092d5a47ff97e85ae9641
Darron Froese <darron@froese.org>
2016-04-28 22:18:08 -0700

    Don't install old version of Docker upstart script. It comes in the package now.

--- recipes/docker.rb
+++ recipes/docker.rb
@@ -27,13 +27,6 @@ cookbook_file '/etc/default/docker' do
   notifies :restart, 'service[docker]', :delayed
 end
 
-cookbook_file '/etc/init/docker.conf' do
-  owner 'root'
-  group 'root'
-  mode 00644
-  notifies :restart, 'service[docker]', :immediately
-end
-
 remote_file '/usr/local/bin/docker-machine' do
   source 'https://github.com/docker/machine/releases/download/v0.3.0/docker-machine_linux-amd64'
   owner 'root'
